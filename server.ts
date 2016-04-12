c953e3084d23735ce002ae59850a0d85a31bb903
tricatseng <heygameover@gmail.com>
2016-04-12 10:46:04 -0700

    added Movie route/model to server

--- server.ts
+++ server.ts
@@ -29,6 +29,8 @@ app.use(cookieParser());
 app.use(express.static('./ngApp'));
 app.use('/scripts', express.static('bower_components'));
 
+app.use('/api/v1/movies', require ('./Movie/routes'));
+
 app.get('/*', function(req, res, next) {
   if (/.js|.html|.css|templates|js|scripts/.test(req.path) || req.xhr) {
     return next({ status: 404, message: 'Not Found' });
