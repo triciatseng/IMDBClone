d55bce70b2e555957307d0ef4f8e37319aa130c7
tricatseng <heygameover@gmail.com>
2016-04-12 16:54:33 -0700

    added comment functions to MovieDetailsController

--- ngApp/js/Controllers/MovieDetailsController.ts
+++ ngApp/js/Controllers/MovieDetailsController.ts
@@ -1,10 +1,26 @@
 namespace app.Controllers{
   export class MovieDetailsController{
     public movie: app.i.IMovie;
+    public comment: app.i.IComment;
 
     public removeMovie(){
       this.MovieService.remove(this.movie._id).then(() => {
         this.$state.go('Home');
+      });
+    }
+
+    public createComment(){
+      this.comment.movie = this.movie._id;
+      this.CommentService.create(this.comment).then((res) => {
+        this.movie.comments.push(res);
+        this.comment.message="";
+        this.comment.user="";
+      });
+    }
+
+    public removeComment(c:app.i.IComment){
+      this.CommentService.remove(c._id).then(() => {
+        this.movie.comments.splice(this.movie.comments.indexOf(c),1);
       });
     }
 
