346baf4e3853e2f4cc9b0b7b2be80c3fcde3b6a7
tricatseng <heygameover@gmail.com>
2016-04-13 13:17:33 -0700

    completed work on fb-passport

--- config/passport.ts
+++ config/passport.ts
@@ -1,30 +1,34 @@
 let passport = require('passport');
 let FacebookStrategy = require('passport-facebook').Strategy;
-import {User} from '../User/model';
+import { User } from '../User/model';
+
 
 passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-  profileFields: ['id','displayName','email'],
-  passReqToCallback: true
-}, function(req,accessToken,refreshToken,profile,next){
-  User.findOne({'facebook.id': profile.id})
-    .exec((err,user) => {
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+    profileFields: ['id', 'displayName', 'email'],
+    passReqToCallback: true
+  },
+  function(req, accessToken, refreshToken, profile, next) {
+    User.findOne({ 'facebook.id': profile.id }).exec((err, user) => {
       if (err) return next(err);
       if (user) {
         req['tempUser'] = user;
-        next(null,user);
+        next(null, user);
       } else {
         let u = new User();
         u.name = profile.displayName;
+        u.email = profile.email;
         u.facebook.id = profile.id;
         u.facebook.token = accessToken;
-        u.save((err,user) => {
+        u.save((err, user) => {
           if (err) return next(err);
           req['tempUser'] = user;
-          next(null,user);
+          next(null, user);
         });
       }
     });
-}));
+  }));
+
+  export = passport;
--- ngApp/js/Controllers/HomeController.ts
+++ ngApp/js/Controllers/HomeController.ts
@@ -6,10 +6,17 @@ namespace app.controllers {
     constructor(
       private MovieService: app.Services.MovieService,
       private UserService: app.Services.UserService,
-      private $state: ng.ui.IStateService
+      private $state: ng.ui.IStateService,
+      private $location: ng.ILocationService
     ){
       this.movies = MovieService.getAll();
       this.user = UserService.status;
+      if($location.search().code){
+        UserService.setToken($location.search().code);
+        UserService.setUser();
+        $location.search('');
+        if($location.hash()) $location.hash('');
+      }
     }
   }
   angular.module('app').controller('HomeController', HomeController);
